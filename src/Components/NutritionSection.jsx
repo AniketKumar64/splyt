4c922f92bfcc61cab076032aa96db0b3b24a3ec8
AniketKumar64 <kumaraniket20805@gmail.com>
2025-11-15 17:42:45 +0530

    Refactor NutritionSection and TestimonialSection components to define nutrientLists and cards locally, removing imports from constants

--- src/Components/NutritionSection.jsx
+++ src/Components/NutritionSection.jsx
@@ -3,12 +3,19 @@ import { useEffect, useState } from "react";
 import { useGSAP } from "@gsap/react";
 import { SplitText } from "gsap/all";
 import gsap from "gsap";
-import { nutrientLists } from "../constants/index.js";
 
 const NutritionSection = () => {
   const isMobile = useMediaQuery({
     query: "(max-width: 768px)",
   });
+
+  const nutrientLists = [
+  { label: "Potassium", amount: "245mg" },
+  { label: "Calcium", amount: "500mg" },
+  { label: "Vitamin A", amount: "176mcg" },
+  { label: "Vitamin D", amount: "5mcg" },
+  { label: "Iron", amount: "1mg" },
+];
 
   const [lists, setLists] = useState(nutrientLists);
 
